e3a019dbd176d010771b65cc9cdac634f767f507
weslyramalho <weslyramalho@yahoo.com.br>
2023-04-04 20:13:42 -0300

    Criado classe pessoa e endereco

---
+++ src/main/java/com/wr/gerencia/entities/Endereco.java
@@ -0,0 +1,79 @@
+package com.wr.gerencia.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_endereco")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String logradouro;
+    private String cep;
+    private Integer numero;
+    private String cidade;
+    private String estado;
+
+
+    public Endereco() {
+    }
+
+    public Endereco(UUID id, String logradouro, String cep, Integer numero, String cidade, String estado) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/wr/gerencia/entities/Pessoa.java
@@ -0,0 +1,74 @@
+package com.wr.gerencia.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String nome;
+    private Date dataNascimento;
+    //  @OneToMany
+    //@JoinColumn(name = "pessoa_id")
+    // private Endereco enderecoPrincipal;
+    // @OneToMany(mappedBy = "pessoa")
+    // private List<Endereco> endereco = new ArrayList<>();
+
+    public Pessoa() {
+    }
+
+    public Pessoa(UUID id, String nome, Date dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        //this.enderecoPrincipal = enderecoPrincipal;
+        //this.endereco = endereco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+/*
+    public Endereco getEnderecoPrincipal() {
+        return enderecoPrincipal;
+    }
+
+    public void setEnderecoPrincipal(Endereco enderecoPrincipal) {
+        this.enderecoPrincipal = enderecoPrincipal;
+    }
+
+    public List<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(List<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+
+ */
+}
