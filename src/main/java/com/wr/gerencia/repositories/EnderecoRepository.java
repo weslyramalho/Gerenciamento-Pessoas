8cfa5c51e5e5c482bfdc2cc5e540ba7e88216f1d
weslyramalho <weslyramalho@yahoo.com.br>
2023-04-04 20:33:29 -0300

    Criada as repositories

---
+++ src/main/java/com/wr/gerencia/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.wr.gerencia.repositories;
+
+import com.wr.gerencia.entities.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, UUID> {
+}
