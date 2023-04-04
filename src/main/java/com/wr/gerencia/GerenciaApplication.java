b8eb81ba42fe9451f79339374a40c82bd3a9899a
weslyramalho <weslyramalho@yahoo.com.br>
2023-04-04 19:57:29 -0300

    iniciado projeto

---
+++ src/main/java/com/wr/gerencia/GerenciaApplication.java
@@ -0,0 +1,13 @@
+package com.wr.gerencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wr/gerencia/GerenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.gerencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
