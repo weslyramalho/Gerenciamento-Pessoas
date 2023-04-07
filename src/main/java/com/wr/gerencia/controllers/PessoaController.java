9dd7b2f33290e2921686e43cce7d0700ee685da5
weslyramalho <weslyramalho@yahoo.com.br>
2023-04-07 17:50:40 -0300

    Controller pessoa ok

--- src/main/java/com/wr/gerencia/controllers/PessoaController.java
+++ src/main/java/com/wr/gerencia/controllers/PessoaController.java
@@ -20,13 +20,30 @@ public class PessoaController {
         List<Pessoa> list = service.listarPessoas();
         return ResponseEntity.ok().body(list);
     }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Pessoa> buscaPorId(@PathVariable Long id){
+        Pessoa obj = service.consultaUmaPessoa(id);
+        return ResponseEntity.ok().body(obj);
+    }
 
     @PostMapping
-    public ResponseEntity<Pessoa> insert(@RequestBody Pessoa obj){
+    public ResponseEntity<Pessoa> inserir(@RequestBody Pessoa obj){
         obj = service.novaPessoa(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    private ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    private ResponseEntity<Pessoa> editar(@PathVariable long id, @RequestBody Pessoa obj){
+        obj = service.editarPessoa(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
